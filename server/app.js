2c07500e61d85594dead0426d111e36d7f9ef0ef
Niccolò Boscolo <niccolo.boscolo88@icloud.com>
2020-12-15 18:49:28 +0100

    17

--- server/app.js
+++ server/app.js
@@ -44,14 +44,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname,"client","build",'index.html'));
-});
-}
 
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 module.exports = app;
