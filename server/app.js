d435a0e05218020112cd478ba38b481ff223cef9
Niccolò Boscolo <niccolo.boscolo88@icloud.com>
2020-12-15 15:06:46 +0100

    first tentative of build app with postScript

--- server/app.js
+++ server/app.js
@@ -42,5 +42,12 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+if(process.env.NODE_ENV ==="production"){
+  app.use(express.static("client/build"))
+  //const path = require("path");
+  app.get("*", (req,res) =>{
+      res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+  })
+}
 
 module.exports = app;
