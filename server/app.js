026b5f128232a0fadbcd8488a4cad34f297fd2b3
Niccolò Boscolo <niccolo.boscolo88@icloud.com>
2020-12-15 15:25:11 +0100

    second tent

--- server/app.js
+++ server/app.js
@@ -42,12 +42,17 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-if(process.env.NODE_ENV ==="production"){
-  app.use(express.static("client/build"))
-  //const path = require("path");
-  app.get("*", (req,res) =>{
-      res.sendFile(path.resolve(__dirname,"client","build","index.html"))
-  })
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 module.exports = app;
